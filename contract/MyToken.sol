312041ea410e0050bc38f191d2e8971049a506b7
Peter <wkchen007@gmail.com>
2025-10-15 16:26:00 +0800

    Add openzeppelin dependency.

--- contract/MyToken.sol
+++ contract/MyToken.sol
@@ -5,9 +5,7 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor()
-        ERC20("MyToken", "MTK")
-    {}
+    constructor() ERC20("MyToken", "MTK") {}
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
